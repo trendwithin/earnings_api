499ae519488c49d965d6a570b20119f114f0ce17
Michael Becco <e4e6d4d5@gmail.com>
2016-05-10 13:17:48 -0700

    Add params[:earnings_beat] to Endpoint

--- app/models/earning.rb
+++ app/models/earning.rb
@@ -1,2 +1,8 @@
 class Earning < ActiveRecord::Base
+
+  def Earning.earnings_beat beat
+    beat = beat.to_f
+    hits = self.select { |e| e[:earnings].to_f > 0 && e[:earnings_last_q].to_f > 0 }
+    hits = hits.select { |e| ( (e[:earnings].to_f - e[:earnings_last_q].to_f)/e[:earnings_last_q].to_f) * 100 >= beat }
+  end
 end
---
+++ test/integration/API/V1/listing_earnings_test.rb
@@ -0,0 +1,28 @@
+require 'test_helper'
+
+class ListingEarningsTest < ActionDispatch::IntegrationTest
+  setup { host! 'api.example.com' }
+
+  test 'return list of earnings' do
+    get '/earnings'
+    assert_equal 200, response.status
+  end
+
+  test 'list filtered by earnings data' do
+    Earning.create!(company_symbol: 'ITEST', earnings_released_on: '2016-05-09 11:51:26', earnings: '0.50', earnings_last_q: '0.25')
+    get '/earnings?earnings_beat=25', format: :json
+    assert_equal Mime::JSON, response.content_type
+    assert_equal 200, response.status
+    earnings = JSON.parse(response.body, symbolize_names: true)
+    assert_equal earnings.size, 1
+    json_response = [{:company_symbol=>"ITEST", :earnings_released_on=>"05/09/2016", :earnings=>"0.50", :expectation=>nil, :earnings_last_q=>"0.25", :revenue=>nil}]
+    assert_equal earnings, json_response
+    company_symbols = earnings.collect { |s| s[:company_symbol] }
+    assert_includes company_symbols, 'ITEST'
+    values = earnings.find { |e| e[:company_symbol] == 'ITEST' }
+    assert_equal 6, values.size
+    assert_equal '05/09/2016', values[:earnings_released_on]
+    assert_equal '0.50', values[:earnings]
+    assert_equal '0.25', values[:earnings_last_q]
+  end
+end
