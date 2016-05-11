6b3832bf55c692fa09cf76ead095d961e080c717
Michael Becco <e4e6d4d5@gmail.com>
2016-05-11 14:47:29 -0700

    Test #earning_beat #expectation_beat returns expected

--- test/models/earning_test.rb
+++ test/models/earning_test.rb
@@ -1,4 +1,28 @@
 require 'test_helper'
 
 class EarningTest < ActiveSupport::TestCase
+
+  test '#earnings_beat returns valid filtered record' do
+    Earning.create!(company_symbol: 'EX', earnings_released_on: '2016-05-09 11:51:26',earnings: '0.5', earnings_last_q: '0.25')
+    beat = Earning.earnings_beat '50'
+    assert beat.to_s.include?('EX')
+  end
+
+  test '#earnings_beat will not return invalid filtered record' do
+    Earning.create!(company_symbol: 'EX', earnings_released_on: '2016-05-09 11:51:26',earnings: '0.5', earnings_last_q: '0.25')
+    beat = Earning.earnings_beat '200'
+    refute beat.to_s.include?('EX')
+  end
+
+  test '#expectation_beat returns valid filtered record' do
+    Earning.create!(company_symbol: 'EX', earnings_released_on: '2016-05-09 11:51:26',earnings: '0.5', expectation: '0.25')
+    beat = Earning.expectation_beat '50'
+    assert beat.to_s.include?('EX')
+  end
+
+  test '#expectation_beat will not return invalid filtered record' do
+    Earning.create!(company_symbol: 'EX', earnings_released_on: '2016-05-09 11:51:26',earnings: '0.5', expectation: '0.25')
+    beat = Earning.expectation_beat '200'
+    refute beat.to_s.include?('EX')
+  end
 end
