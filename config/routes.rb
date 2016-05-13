59a3dc2c4eb39f2cf1f68285f4f073064d573091
Michael Becco <e4e6d4d5@gmail.com>
2016-05-13 16:07:02 -0700

    Add Boostrap, Earnings_Controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 require 'api_version'
 
 Rails.application.routes.draw do
+  root 'earnings#index'
+  resources :earnings, only: [:index]
   namespace :api, defaults: { format: :json }, constraints: { subdomain: 'api' },
   path: '/' do
     scope module: :v1, constraints: ApiVersion.new(version: 1, default: true) do
       resources :earnings, only: [:index]
     end
   end
+
 end
