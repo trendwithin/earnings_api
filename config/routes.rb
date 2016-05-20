8fc7e79d379b4946e80e7483077fe3bacdeaac00
Michael Becco <e4e6d4d5@gmail.com>
2016-05-20 12:23:55 -0700

    Add JQuery/AJAX Earnings#retrieve

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,11 @@ require 'api_version'
 
 Rails.application.routes.draw do
   root 'earnings#index'
-  resources :earnings, only: [:index]
+  resources :earnings, only: [:index] do
+    collection do
+      get 'retrieve'
+    end
+  end
   namespace :api, defaults: { format: :json }, constraints: { subdomain: 'api' },
   path: '/' do
     scope module: :v1, constraints: ApiVersion.new(version: 1, default: true) do
